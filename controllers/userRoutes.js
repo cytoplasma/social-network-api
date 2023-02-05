173de892dfab2023260eb85faaa502fbc27edfb1
cytoplasma <jpiromg@gmail.com>
2023-02-04 23:05:16 -0800

    add functionality for add/del reactions, return not found resp statements, thoughtRoutes in index.js, userRoutes.js add/del

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,56 +2,75 @@ const router = require('express').Router();
 const { User, Thought } = require('../models');
 
 router.get('/', async (req, res) => {
-  try {
-    const userData = await User.find({}).select('-__v');
-    res.json(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+    try {
+      const userData = await User.find({}).select('-__v');
+      res.json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/:id', async (req, res) => {
-  try {
-    const userData = await User.findById(req.params.id).select('-__v').populate("thoughts friends");
-    if (!userData) {
-      res.json("User with given id does not exist.");
-    } else {
+    try {
+      const userData = await User.findById(req.params.id).select('-__v').populate("thoughts friends");
+      if (!userData) {
+        return res.status(404).json("User with given id does not exist");
+      }
       res.json(userData);
+    } catch (err) {
+        res.status(500).json(err);
     }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const newUser = await User.create(req.body);
-    res.json(newUser);
-  } catch(err) {
-    res.status(400).json(err);
-  }
 });
 
 router.put('/:id', async (req, res) => {
-  try {
-    await User.findByIdAndUpdate(req.params.id, req.body);
-    res.sendStatus(200);
-  } catch(err) {
-    res.status(500).json(err);
-  }
+    try {
+        const userData = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!userData) {
+            return res.status(404).json("User with given id does not exist");
+        }
+        res.sendStatus(200);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 });
 
 router.delete('/:id', async (req, res) => {
-  try {
-    const userData = await User.findById(req.params.id);
-    if (userData.thoughts.length > 0) {
-      await Thought.deleteMany({username: userData.username});
+    try {
+        const userData = await User.findByIdAndDelete(req.params.id);
+        if (!userData) {
+            return res.status(404).json("User with given id does not exist");
+        }
+        if (userData.thoughts.length >  0) {
+            await Thought.deleteMany({username: userData.username});
+        }
+        res.sendStatus(200);
+    } catch(err) {
+        res.status(400).json(err);
     }
-    await User.findByIdAndDelete(req.params.id);
-    res.sendStatus(200);
-  } catch(err) {
-    res.status(400).json(err);
-  }
+});
+
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const userData = await User.findByIdAndUpdate(req.params.userId, { $addToSet: { friends: req.params.friendId }}, { new: true });
+        if (!userData) {
+            return res.status(404).json("User with given id does not exist");
+        }
+        res.sendStatus(200);
+    } catch(err) {
+        res.json(400).json(err);
+    }
+});
+
+router.delete('/:userId/frineds/:friendId', async (req, res) => {
+    try {
+        const userData = await User.findByIdAndUpdate(req.params.userId, { $pull: { friends: req.params.friendId }}, { new: true });
+        if (!userData) {
+            return res.status(404).json("User with given id does not exist");
+        }
+        res.sendStatus(200);
+    } catch(err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
--- models/Reaction.js
+++ models/Reaction.js
@@ -1,24 +1,28 @@
-const { Schema, Types } = require('mongoose');
-
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: new Types.ObjectId()
+const { Schema, Types } = require("mongoose");
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: [280, "Cannot be longer than 280 characters"],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (timestamp) => timestamp.toLocaleString(),
+    },
   },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength:[280, 'Cannot be longer than 280 characters']
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    get: timestamp =>  timestamp.toLocaleString()
+  {
+    id: false,
   }
-})
+);
 
 module.exports = reactionSchema;
